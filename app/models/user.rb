a44c2dda9586572c6bf74eef4364211289c5f3c9
Samuel Kan <wenhung.kan2012@yahoo.com>
2012-10-31 14:24:28 +0800

    Modify before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   # callback before saving to database
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

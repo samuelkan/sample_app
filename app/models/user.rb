824e270c31b4f25c31525004f094edfeab8d5ee8
Samuel Kan <wenhung.kan2012@yahoo.com>
2012-11-05 11:23:05 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   
   # callback before saving to database
   before_save { self.email.downcase! }
@@ -24,6 +25,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    microposts
+  end
 
   private
 
